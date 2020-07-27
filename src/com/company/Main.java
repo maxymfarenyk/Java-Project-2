758aa79878732bcf7891155bf9a99b8362be973c
Farenyk Maximko <maximfarenik@gmail.com>
2020-07-27 19:11:31 +0300

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+import java.lang.String;
+public class Main {
+
+    public static void main(String[] args) {
+	    byte num = 23;
+	    int num_1 = 1253734;
+
+	    float num_2 = 23.45f;
+	    double num_3 = 124.235123623d;
+
+	    boolean isTrue = true;
+	    char sym = 'R';
+
+	    String str = "Hello world";
+
+	    System.out.print(str);
+    }
+}
